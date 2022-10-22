0030e79fbfb21cfafe7bd3a31e232beb0e4922bb
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2022-10-23 01:16:36 +0300

    adding timer

--- internal/service/session/handlers/blocks.go
+++ internal/service/session/handlers/blocks.go
@@ -1,18 +1,33 @@
 package handlers
 
-// BlockHandler listens to the new blocks received from chanel and upgrading the timer state
+import "gitlab.com/rarify-protocol/tss-svc/internal/service/session"
+
+// BlockHandler is listening to the new blocks received from chanel and upgrading the timer state
 type BlockHandler struct {
-	op <-chan uint64
+	op    <-chan uint64
+	timer *session.Timer
 }
 
-func NewBlockHandler(op <-chan uint64) *BlockHandler {
+func NewBlockHandler(op <-chan uint64, t *session.Timer) *BlockHandler {
 	s := &BlockHandler{
-		op: op,
+		op:    op,
+		timer: t,
 	}
+
+	s.listen()
 
 	return s
 }
 
 func (o *BlockHandler) listen() {
+	go func() {
+		for {
+			height, ok := <-o.op
+			if !ok {
+				break
+			}
 
+			o.timer.NewBlock(height)
+		}
+	}()
 }
--- internal/service/session/timer.go
+++ internal/service/session/timer.go
@@ -1,3 +1,42 @@
 package session
 
-// TODO
+import (
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/rarify-protocol/tss-svc/internal/config"
+)
+
+type Notifier func(height uint64) error
+
+type Timer struct {
+	currentBlock uint64
+	toNotify     map[string]Notifier
+	log          *logan.Entry
+}
+
+func NewTimer(cfg config.Config) *Timer {
+	return &Timer{
+		toNotify: make(map[string]Notifier),
+		log:      cfg.Log(),
+	}
+}
+
+func (t *Timer) NewBlock(height uint64) {
+	t.currentBlock = height
+	go t.notify(height)
+}
+
+func (t *Timer) notify(height uint64) {
+	for name, f := range t.toNotify {
+		if err := f(height); err != nil {
+			t.log.WithError(err).Errorf("got an error notifying %s", name)
+		}
+	}
+}
+
+func (t *Timer) CurrentBlock() uint64 {
+	return t.currentBlock
+}
+
+func (t *Timer) SubscribeToNotification(name string, f Notifier) {
+	t.toNotify[name] = f
+}
