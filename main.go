cecd921f8fc4a08fd0caefdd599797eb8a2b99f6
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-01-11 21:43:48 +0200

    configuring GOMAXPROCS

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"os"
+	"runtime"
 
 	"gitlab.com/rarimo/tss/tss-svc/internal/cli"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	if !cli.Run(os.Args) {
 		os.Exit(1)
 	}
