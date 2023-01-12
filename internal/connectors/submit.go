2f59a7a573abb8884e0d7acf08dd903e7e7cf993
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-01-12 21:27:11 +0200

    fix broadcast clients buffer

--- internal/connectors/submit.go
+++ internal/connectors/submit.go
@@ -19,29 +19,25 @@ type con struct {
 	lastUsed time.Time
 }
 
+var clientsBuffer = struct {
+	mu      sync.Mutex
+	clients map[string]*con
+}{
+	clients: make(map[string]*con),
+}
+
 // SubmitConnector submits signed requests to the party.
 // Also holds buffer of connections to reduce submitting time.
 type SubmitConnector struct {
-	mu       sync.Mutex
-	isClosed bool
-	secret   *secret.TssSecret
-	clients  map[string]*con
+	secret *secret.TssSecret
 }
 
 func NewSubmitConnector(secret *secret.TssSecret) *SubmitConnector {
 	c := &SubmitConnector{
-		isClosed: false,
-		secret:   secret,
-		clients:  make(map[string]*con),
+		secret: secret,
 	}
 
 	return c
-}
-
-func (s *SubmitConnector) Close() error {
-	s.isClosed = true
-	s.cleanAll()
-	return nil
 }
 
 func (s *SubmitConnector) Submit(ctx context.Context, party rarimo.Party, request *types.MsgSubmitRequest) (*types.MsgSubmitResponse, error) {
@@ -49,8 +45,8 @@ func (s *SubmitConnector) Submit(ctx context.Context, party rarimo.Party, reques
 		return nil, err
 	}
 
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	clientsBuffer.mu.Lock()
+	defer clientsBuffer.mu.Unlock()
 
 	client, err := s.getClient(party.Address)
 	if err != nil {
@@ -61,11 +57,7 @@ func (s *SubmitConnector) Submit(ctx context.Context, party rarimo.Party, reques
 }
 
 func (s *SubmitConnector) getClient(addr string) (*grpc.ClientConn, error) {
-	if err := s.closed(); err != nil {
-		return nil, err
-	}
-
-	if client, ok := s.clients[addr]; ok && client != nil {
+	if client, ok := clientsBuffer.clients[addr]; ok && client != nil {
 		client.lastUsed = time.Now().UTC()
 		return client.client, nil
 	}
@@ -75,25 +67,10 @@ func (s *SubmitConnector) getClient(addr string) (*grpc.ClientConn, error) {
 		return nil, err
 	}
 
-	s.clients[addr] = &con{
+	clientsBuffer.clients[addr] = &con{
 		client:   client,
 		lastUsed: time.Now().UTC(),
 	}
 
 	return client, nil
-}
-
-func (s *SubmitConnector) cleanAll() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	for _, c := range s.clients {
-		c.client.Close()
-	}
-}
-
-func (s *SubmitConnector) closed() error {
-	if s.isClosed {
-		return ErrorConnectorClosed
-	}
-	return nil
 }
